c624c19dbcd2de86db5eaeeacdf87f7bce152c20
Ruinedmango <00rudystx@gmail.com>
2025-09-02 23:50:17 -0800

    Finished tiered loot tables

--- src/main/java/com/ruinedmango/mazed/datagen/MazedLootTableProvider.java
+++ src/main/java/com/ruinedmango/mazed/datagen/MazedLootTableProvider.java
@@ -48,7 +48,8 @@ public class MazedLootTableProvider implements LootTableSubProvider {
 				LootTable.lootTable().apply(SetItemCountFunction.setCount(ConstantValue.exactly(30)))
 						.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(10, 20))
 								.setBonusRolls(ConstantValue.exactly(3)).add(LootItem.lootTableItem(Items.DIAMOND))
-								.add(LootItem.lootTableItem(Items.DIAMOND_PICKAXE))));
+								.add(LootItem.lootTableItem(Items.DIAMOND_PICKAXE))
+								.add(LootItem.lootTableItem(Items.ENCHANTED_GOLDEN_APPLE))));
 		output.accept(MAZE_CHEST_TIER1, LootTable.lootTable()
 				.apply(new EnchantRandomlyFunction.Builder()
 						.withEnchantment(lookupProvider.getOrThrow(Enchantments.UNBREAKING)))
@@ -58,7 +59,59 @@ public class MazedLootTableProvider implements LootTableSubProvider {
 						.add(LootItem.lootTableItem(Items.COBBLESTONE)))
 				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1, 2))
 						.add(LootItem.lootTableItem(Items.STICK)).add(LootItem.lootTableItem(Items.WOODEN_PICKAXE))));
-
+		output.accept(MAZE_CHEST_TIER2, LootTable.lootTable()
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1, 2))
+						.add(LootItem.lootTableItem(Items.STICK)).add(LootItem.lootTableItem(Items.STONE_PICKAXE)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(12, 14))
+						.add(LootItem.lootTableItem(Items.STONE)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 5))
+						.add(LootItem.lootTableItem(Items.BEETROOT)).add(LootItem.lootTableItem(Items.BEETROOT_SOUP))
+						.add(LootItem.lootTableItem(Items.BREAD))));
+		output.accept(MAZE_CHEST_TIER3, LootTable.lootTable()
+				.apply(new EnchantRandomlyFunction.Builder()
+						.withEnchantment(lookupProvider.getOrThrow(Enchantments.EFFICIENCY)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1, 2))
+						.add(LootItem.lootTableItem(Items.STONE_PICKAXE)).add(LootItem.lootTableItem(Items.IRON_INGOT)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(12, 14))
+						.add(LootItem.lootTableItem(Items.OAK_PLANKS)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 7))
+						.add(LootItem.lootTableItem(Items.MELON_SLICE)).add(LootItem.lootTableItem(Items.BREAD))
+						.add(LootItem.lootTableItem(Items.BEETROOT_SOUP))));
+		output.accept(MAZE_CHEST_TIER4, LootTable.lootTable()
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1, 3))
+						.add(LootItem.lootTableItem(Items.COAL)).add(LootItem.lootTableItem(Items.IRON_INGOT)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(0, 1))
+						.add(LootItem.lootTableItem(Items.IRON_PICKAXE)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(12, 14))
+						.add(LootItem.lootTableItem(Items.OAK_PLANKS)).add(LootItem.lootTableItem(Items.BRICKS)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 8))
+						.add(LootItem.lootTableItem(Items.MELON_SLICE)).add(LootItem.lootTableItem(Items.BREAD))
+						.add(LootItem.lootTableItem(Items.BEETROOT_SOUP))));
+		output.accept(MAZE_CHEST_TIER5,
+				LootTable.lootTable()
+						.apply(new EnchantRandomlyFunction.Builder()
+								.withEnchantment(lookupProvider.getOrThrow(Enchantments.EFFICIENCY)))
+						.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1, 5))
+								.add(LootItem.lootTableItem(Items.DIAMOND)).add(LootItem.lootTableItem(Items.COAL))
+								.add(LootItem.lootTableItem(Items.IRON_INGOT)))
+						.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(0, 1))
+								.add(LootItem.lootTableItem(Items.IRON_PICKAXE)))
+						.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(12, 14))
+								.add(LootItem.lootTableItem(Items.BRICKS)))
+						.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 8))
+								.add(LootItem.lootTableItem(Items.MUSHROOM_STEW))
+								.add(LootItem.lootTableItem(Items.RABBIT_STEW))
+								.add(LootItem.lootTableItem(Items.BEETROOT_SOUP))));
+		output.accept(MAZE_CHEST_TIER6, LootTable.lootTable()
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 5))
+						.add(LootItem.lootTableItem(Items.DIAMOND)).add(LootItem.lootTableItem(Items.IRON_INGOT)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(0, 1))
+						.add(LootItem.lootTableItem(Items.DIAMOND_PICKAXE)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(12, 14))
+						.add(LootItem.lootTableItem(Items.COBBLED_DEEPSLATE)))
+				.withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3, 8))
+						.add(LootItem.lootTableItem(Items.GOLDEN_CARROT))
+						.add(LootItem.lootTableItem(Items.GOLDEN_APPLE)).add(LootItem.lootTableItem(Items.CAKE))));
 	}
 
 }
